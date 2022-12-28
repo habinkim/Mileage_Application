376c64a1a1349b82cff991fd67161829cd156a42
habin <danny9643@naver.com>
2022-12-28 09:51:14 +0900

    SwaggerOpenApiConfig 수정

--- src/main/java/com/habin/marketboro_mileage_task/module/config/SwaggerOpenApiConfig.java
+++ src/main/java/com/habin/marketboro_mileage_task/module/config/SwaggerOpenApiConfig.java
@@ -5,14 +5,12 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static java.time.LocalDate.now;
 
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerOpenApiConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -21,25 +19,6 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedOrigins("*")
                 .exposedHeaders("TOKEN");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/index.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/", ".jsp");
     }
 
     @Bean
